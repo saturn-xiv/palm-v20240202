7adf7e6252dd04e7c52b8e2d3744ec2ef434e387
Jeremy Zheng <jitang.zheng@gmail.com>
2023-12-15 03:07:50 +0000

    :tada: create gourd project

---
+++ scripts/schema.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+export PROTOBUF_ROOT=$HOME/.local
+export WORKSPACE=$PWD
+export PALM_PROTOCOLS=$WORKSPACE/protocols
+
+function generate_goard() {
+    cd $WORKSPACE
+
+    echo 'generate code for goard'
+    local target=$WORKSPACE/gourd
+    if [ -d $target ]; then
+        rm -r $target
+    fi
+    mkdir -p $target/include/palm $target/src
+    thrift -out $target --gen cpp:no_skeleton -r $PALM_PROTOCOLS/*.thrift
+
+    $PROTOBUF_ROOT/bin/protoc -I $PALM_PROTOCOLS \
+        -I $PROTOBUF_ROOT/include/google/protobuf \
+        --cpp_out=$target --grpc_out=$target \
+        --plugin=protoc-gen-grpc=$PROTOBUF_ROOT/bin/grpc_cpp_plugin \
+        $PALM_PROTOCOLS/*.proto
+    mv $target/*.h $target/include/palm/
+    mv $target/*.cc $target/*.cpp $target/src/
+}
+
+generate_goard
+
+echo 'done.'
+exit 0
