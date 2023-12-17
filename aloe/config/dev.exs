f4338069881c6902b2dea77932dead35e11761b1
Jeremy Zheng <jitang.zheng@gmail.com>
2023-12-17 04:57:19 +0000

    :wrench: change db dir

--- aloe/config/dev.exs
+++ aloe/config/dev.exs
@@ -2,7 +2,7 @@ import Config
 
 # Configure your database
 config :aloe, Aloe.Repo,
-  database: Path.expand("../aloe_dev.db", Path.dirname(__ENV__.file)),
+  database: Path.expand("../tmp/aloe.db", Path.dirname(__ENV__.file)),
   pool_size: 5,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true
