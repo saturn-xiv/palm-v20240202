a24cdf75ef7622627e80e17430ccf75492980b90
Jeremy Zheng <jitang.zheng@gmail.com>
2023-12-29 00:41:44 +0000

    :globe_with_meridians: add format support

--- aloe/lib/aloe_web/controllers/user/session_controller.ex
+++ aloe/lib/aloe_web/controllers/user/session_controller.ex
@@ -2,11 +2,14 @@ defmodule AloeWeb.User.SessionController do
   use AloeWeb, :controller
 
   def new(conn, _params) do
-    render(conn, :new, layout: false)
+    render(conn, :new,
+      layout: false,
+      page_title: AloeWeb.Gettext.dgettext("pages", "users.sign-in.title")
+    )
   end
 
-  def create(conn, _params) do
-    render(conn, :home)
+  def create(user, params \\ :empty) do
+    IO.puts("#{user}")
   end
 
   def delete(conn, _params) do
